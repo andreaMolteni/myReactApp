0a8d1c2e842b8f192d9d09765509e40e7070b1a6
Andrea Molteni <molteni.engineer@gmail.com>
2021-09-21 09:21:12 +0200

    primo commit: impostata vista dei todo

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
 import './App.css';
 
+
+const todosInit = [
+  {
+    name: 'Studiare React',
+    dueDate: new Date().toLocaleDateString(),
+    user_id: 1,
+  },
+  {
+    name: 'Studiare Symfony',
+    dueDate: new Date().toLocaleDateString(),
+    user_id: 2,
+  },
+  {
+    name: 'Studiare Docker',
+    dueDate: new Date().toLocaleDateString(),
+    user_id: 3,
+  },
+  {
+    name: 'Studiare AWS Services',
+    dueDate: new Date().toLocaleDateString(),
+    user_id: 4,
+  }
+]
+
 function App() {
+  const [todos, setTodos] = useState(todosInit)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="App container-fluid">
+      <h1>MY TODOS LIST:</h1>
+      <div className="card todos-cards-wrapper" style={{width: '18rem'}}>
+        <div className="card-header">
+          TODO:
+        </div>
+        <ul class="list-group list-group-flush">
+          {todos.map(todo => <li className="list-group-item">{todo.name}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
