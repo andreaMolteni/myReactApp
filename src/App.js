6dcbe1123349fea6065b7d2b9cce21faccf48085
Andrea Molteni <molteni.engineer@gmail.com>
2021-11-18 00:21:50 +0100

    aggiunta navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addTodo, fetchTodos } from './features/todos/todoSlice';
 import { filterTodo } from './features/todos/filterSlice';
-import MyLists from './features/lists/myLists'
+import MyLists from './features/lists/myLists';
+import Header from './components/Header';
 import MyTodos from './features/todos/myTodos';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {  // dispatch è tra le prorprietà del componente App
@@ -50,30 +51,32 @@ function App() {  // dispatch è tra le prorprietà del componente App
   const todoEl = useRef('');
 
   return (
-    <div className="App container-fluid">
-      <BrowserRouter>
-        <div className="row d-flex justify-content-center  text-center">
-          <Routes>
-            <Route path="/todos" element={
-              <MyTodos
-                todoEl={todoEl}
-                manageAddTodoClick={manageAddTodoClick}
-                todos={todos} 
-                onFilter={onFilterTodo}
-                filter={filter}
+    <>
+    <BrowserRouter>
+      <Header />
+      <div className="App container-fluid">
+          <div className="row d-flex justify-content-center  text-center">
+            <Routes>
+              <Route path="/todos" element={
+                <MyTodos
+                  todoEl={todoEl}
+                  manageAddTodoClick={manageAddTodoClick}
+                  todos={todos}
+                  onFilter={onFilterTodo}
+                  filter={filter}
+                />
+              }
               />
-            }
-            />
-            <Route exact path="/" element={
-              <MyLists
-                
-              />
-            }
-            />
-          </Routes>
-        </div>
+              <Route exact path="/lists" element={<MyLists/>}/>
+              <Route path="/" element={<MyLists/>} />
+              <Route path="/home" element={<MyLists/>} />
+              {/* <Route path="/" element={<Navigate replace to="/lists" />} />
+              <Route path="/home" element={<Navigate replace to="/lists" />} /> */}
+            </Routes>
+          </div>
+      </div>
       </BrowserRouter>
-    </div>
+    </>
   );
 }
 
--- src/features/todos/todos.js
+++ src/features/todos/todos.js
@@ -5,26 +5,31 @@ import { useDispatch } from 'react-redux';
 
 
 
-export default function Todos ({todos}){
+export default function Todos({ todos }) {
 
     const dispatch = useDispatch()
 
 
     // esempio di cattura degli errori in chiamata asincrona
     const onToggle = async (todo) => {
-        let newTodo = {...todo, completed: !todo.completed} // esempio per provocare errore
+        let newTodo = { ...todo, completed: !todo.completed } // esempio per provocare errore
         // let newTodo = {...todo, completed: !todo.completed}
         try {
             const res = dispatch(toggleTodo(newTodo)).unwrap();
-            console.log('res: ',res);
-        } catch(e){
+            console.log('res: ', res);
+        } catch (e) {
             console.log();
         }
-        
+
     }
     // throw new Error('error');
 
-    return  <ul className="list-group list-group-flush">
-                {todos.map(todo => <Todo todoItem={todo} key={todo.id} toggleTodo={onToggle} removeTodo={() => dispatch(removeTodo(todo))}/>)}
-            </ul>
+    return <ul className="list-group list-group-flush">
+        {todos.map(todo => <Todo
+            todoItem={todo}
+            key={todo.id}
+            toggleTodo={onToggle}
+            removeTodo={() => dispatch(removeTodo(todo))}
+        />)}
+    </ul>
 }
